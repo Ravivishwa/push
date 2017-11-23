6ebe88637847cc840675e05c59188a2c75afc3e2
Ravivishwa <raviv.vishwakarma@gmail.com>
2017-11-23 11:16:30 +0530

    Update notification.js

--- js/notification.js
+++ js/notification.js
@@ -4,7 +4,9 @@ var text='';
 //console.log(data1);
 function sendPushToMyself() {
 	text= $('.container .wi').text().trim();
-	
+	if(text==""){
+		text='+28°C  Mumbai, India';
+	}
 	var isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
 	var isSafari = ('safari' in window && 'pushNotification' in window.safari);
 	var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
